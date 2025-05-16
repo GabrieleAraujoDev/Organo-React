c73f848b91f77e38063761ba2a62b21a7c0d0138
Gabriele Araújo <gabriele.carla.ca@gmail.com>
2025-05-16 19:35:12 -0300

    Banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './componentes/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Banner />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edite <code>src/App.js</code> e salve para recarregar.
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
